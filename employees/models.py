fccbbe1902b89118f71f02985dd3c1e6e3af8bb0
maor leyb <maor.leybo@gmail.com>
2023-03-18 20:44:11 +0200

    added Color, Department, Employees models

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Color(models.Model):
+    # id primary key is created automaticly
+    name = models.CharField(max_length=200)
+    
+
+class Department(models.Model):
+    # id primary key is created automaticly
+    name = models.CharField(max_length=200)
+    
+
+class Employees(models.Model):
+    # id primary key is created automaticly
+    name           = models.CharField(max_length=200)
+    age            = models.IntegerField()
+    is_senior      = models.BooleanField(default=True)   # as requested in the wor document
+    favorite_color = models.ForeignKey(Color, on_delete=models.CASCADE, default=2) # TODO: change cascade to a better function so it wont delete the object
+    Department     = models.ForeignKey(Department, on_delete=models.CASCADE) # TODO: change cascade to a better function so it wont delete the object
+    
+
+    
+
