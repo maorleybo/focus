e68bf7e638bfc6758d6d653fe87f175ef34f48e7
maor leyb <maor.leybo@gmail.com>
2023-03-19 03:35:54 +0200

    added homepage and details views

--- employees/urls.py
+++ employees/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('test/', views.test, name = 'test'),
+    # path('dev/', views.development, name = 'development'),
+    # path('fin/', views.finance, name = 'finance'),
+
+    path('dept/<int:dept_id>/', views.details, name = 'details'),
+
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Department
+
 # Create your views here.
 
+# homepage where you choose a department 
 def index(request):
-    return HttpResponse("Hello world!")
+    lst_of_deps = Department.objects.order_by('id')   #get list of ids
+    context = {'lst_of_deps' : lst_of_deps} 
+    return render(request, 'employees/index.html', context)
+
+
+def test(request):
+    return HttpResponse("This is a Test!")
+
+
+# show emps in the department once you chose a department 
+def details(request, dept_id):
+    strg = "<h1>details for dept %s</h1>"
+    return HttpResponse(strg % dept_id)
+
+#different approch
+
+# def development(request):
+#     return HttpResponse("<h1>Development department</h1>")
+
+
+# def finance(request):
+#     return HttpResponse("<h1>Finance department</h1>")
 
